60f31714937ab25d999a46fabeb293e48d2b43dc
Joseph W. Joshua <joejoshw@gmail.com>
2014-03-28 13:58:45 +0300

    TESTING New Laptop

--- universal/qtxlsx/build-win/lib/cmake/Qt5Xlsx/ExtraSourceIncludes.cmake
+++
@@ -1,7 +0,0 @@
-
-list(APPEND _Qt5Xlsx_OWN_INCLUDE_DIRS
-    "E:/Programming/Qt/smarterp/source/universal/qtxlsx/include" "E:/Programming/Qt/smarterp/source/universal/qtxlsx/include/QtXlsx"
-)
-set(Qt5Xlsx_PRIVATE_INCLUDE_DIRS
-    "E:/Programming/Qt/smarterp/source/universal/qtxlsx/include/QtXlsx/0.2.0" "E:/Programming/Qt/smarterp/source/universal/qtxlsx/include/QtXlsx/0.2.0/QtXlsx"
-)
